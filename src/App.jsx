40d8c15e18afca05d0712891ea4cd1afb4ce6946
Saman <saman.rampage@gmail.com>
2022-12-17 21:56:27 +0330

    Created layout

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { Button } from 'react-bootstrap';
+
+function App() {
+  return (
+    <>
+      {/* Navbar */}
+        <Button variant='info'>
+          Click Me
+        </Button>
+        <Button variant='danger'>
+          Click Me
+        </Button>
+        <Button variant='success'>
+          Click Me
+        </Button>
+      {/* Showcase */}
+    </>
+  );
+}
+
+export default App;
