4bec54b1904695c9c5ad9d23b1bd6b227c7bbe58
Saman <saman.rampage@gmail.com>
2022-12-17 22:02:38 +0330

    Deploy react app

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Layout from './components/layout';
+import Layout from './layout';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
